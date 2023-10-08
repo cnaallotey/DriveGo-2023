1967b93c3e238b967c3398f9c19d006b1278fedc
Michael Kwabena Kissi <rapcedy@gmail.com>
2023-10-08 10:42:33 +0000

    add landing page hero

--- components/LandingHero.vue
+++ components/LandingHero.vue
@@ -1,6 +1,117 @@
 <template>
-    <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M235.58,128.84,160,91.06V48a32,32,0,0,0-64,0V91.06L20.42,128.84A8,8,0,0,0,16,136v32a8,8,0,0,0,9.57,7.84L96,161.76v18.93L82.34,194.34A8,8,0,0,0,80,200v32a8,8,0,0,0,11,7.43l37-14.81,37,14.81A8,8,0,0,0,176,232V200a8,8,0,0,0-2.34-5.66L160,180.69V161.76l70.43,14.08A8,8,0,0,0,240,168V136A8,8,0,0,0,235.58,128.84ZM224,158.24l-70.43-14.08A8,8,0,0,0,144,152v32a8,8,0,0,0,2.34,5.66L160,203.31v16.87l-29-11.61a8,8,0,0,0-5.94,0L96,220.18V203.31l13.66-13.65A8,8,0,0,0,112,184V152a8,8,0,0,0-9.57-7.84L32,158.24v-17.3l75.58-37.78A8,8,0,0,0,112,96V48a16,16,0,0,1,32,0V96a8,8,0,0,0,4.42,7.16L224,140.94Z"></path></svg>
-        hello
-    </div>
+  <div>
+    <section class="relative py-10 md:py-20 lg:py-20 pb-20 lg:pb-20 overflow-hidden">
+      <div class="w-full h-full flex-col justify-start items-center gap-20 inline-flex">
+        <div class="flex-col justify-start items-center gap-20 flex">
+          <div class="flex-col justify-start items-center gap-[13px] flex">
+            <div class="relative">
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#64748B" viewBox="0 0 256 256">
+                <path
+                  d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z">
+                </path>
+              </svg>
+            </div>
+            <div class="w-[73%] text-center">
+              <span class="text-slate-900 text-[40px] font-martelsans font-semibold break-words">All
+                in one platform to
+              </span>
+              <span class="text-green-500 text-[40px] font-martelsans font-semibold break-words">buy
+                and maintain EV vehicles
+              </span>
+              <span class="text-slate-900 text-[40px] font-martelsans font-semibold break-words">
+                from vetted suppliers, secure flexible
+              </span>
+              <span class="text-green-500 text-[40px] font-martelsans font-semibold break-words">financing
+              </span>
+              <span class="text-slate-900 text-[40px] font-martelsans font-semibold break-words">,
+              </span>
+              <span class="text-green-500 text-[40px] font-martelsans font-semibold break-words">maintenance
+              </span>
+              <span class="text-slate-900 text-[40px] font-martelsans font-semibold break-words">
+                and
+              </span>
+              <span class="text-green-500 text-[40px] font-martelsans font-semibold break-words">insurance
+              </span>
+              <span class="text-slate-900 text-[40px] font-martelsans font-semibold break-words">.
+              </span>
+            </div>
+            <div
+              class="pl-10 pr-10 pt-5 pb-5 bg-slate-900 rounded-[60px] justify-center items-center gap-2.5 flex cursor-pointer">
+              <div class="relative">
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ffffff" viewBox="0 0 256 256">
+                    <path
+                      d="M231.79,187.33A80,80,0,0,0,169.57,72.59,80,80,0,1,0,24.21,139.33l-7.66,26.82a14,14,0,0,0,17.3,17.3l26.82-7.66a80.15,80.15,0,0,0,25.75,7.63,80,80,0,0,0,108.91,40.37l26.82,7.66a14,14,0,0,0,17.3-17.3ZM61.53,159.23a8.22,8.22,0,0,0-2.2.3l-26.41,7.55,7.55-26.41a8,8,0,0,0-.68-6,63.95,63.95,0,1,1,25.57,25.57A7.94,7.94,0,0,0,61.53,159.23Zm154,29.44,7.55,26.41-26.41-7.55a8,8,0,0,0-6,.68,64.06,64.06,0,0,1-86.32-24.64A79.93,79.93,0,0,0,174.7,89.71a64,64,0,0,1,41.51,92.93A8,8,0,0,0,215.53,188.67Z">
+                    </path>
+                  </svg>
+                </div>
+              </div>
+              <div class="text-white text-sm font-martelsans font-normal break-words">
+                Speak to an expert
+              </div>
+            </div>
+          </div>
+          <div class="w-[1278px] h-[436px] relative">
+            <div class="w-[1278px] h-[436px] left-0 top-0 absolute">
+            </div>
+            <img class="w-[1278px] h-[436px] left-[-1px] top-0 absolute" src="https://via.placeholder.com/1279x436" />
+          </div>
+        </div>
+        <div class="w-full h-[72px] relative">
+          <div class="w-full h-[60px] left-0 top-[-9px] absolute; bg-slate-900">
+            <div class="left-6 top-3.5 absolute justify-start items-center gap-10 inline-flex">
+              <div class="justify-center items-center flex">
+                <div class="relative">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#22C55E" viewBox="0 0 256 256">
+                    <path
+                      d="M215.79,118.17a8,8,0,0,0-5-5.66L153.18,90.9l14.66-73.33a8,8,0,0,0-13.69-7l-112,120a8,8,0,0,0,3,13l57.63,21.61L88.16,238.43a8,8,0,0,0,13.69,7l112-120A8,8,0,0,0,215.79,118.17ZM109.37,214l10.47-52.38a8,8,0,0,0-5-9.06L62,132.71l84.62-90.66L136.16,94.43a8,8,0,0,0,5,9.06l52.8,19.8Z">
+                    </path>
+                  </svg>
+                </div>
+                <div class="text-green-500 text-2xl font-martelsans font-bold break-words">
+                  The future is electric
+                </div>
+              </div>
+              <div class="justify-center items-center flex">
+                <div class="relative">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#22C55E" viewBox="0 0 256 256">
+                    <path
+                      d="M215.79,118.17a8,8,0,0,0-5-5.66L153.18,90.9l14.66-73.33a8,8,0,0,0-13.69-7l-112,120a8,8,0,0,0,3,13l57.63,21.61L88.16,238.43a8,8,0,0,0,13.69,7l112-120A8,8,0,0,0,215.79,118.17ZM109.37,214l10.47-52.38a8,8,0,0,0-5-9.06L62,132.71l84.62-90.66L136.16,94.43a8,8,0,0,0,5,9.06l52.8,19.8Z">
+                    </path>
+                  </svg>
+                </div>
+                <div class="text-green-500 text-2xl font-martelsans font-bold break-words">
+                  The future is electric
+                </div>
+              </div>
+              <div class="justify-center items-center flex">
+                <div class="relative">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#22C55E" viewBox="0 0 256 256">
+                    <path
+                      d="M215.79,118.17a8,8,0,0,0-5-5.66L153.18,90.9l14.66-73.33a8,8,0,0,0-13.69-7l-112,120a8,8,0,0,0,3,13l57.63,21.61L88.16,238.43a8,8,0,0,0,13.69,7l112-120A8,8,0,0,0,215.79,118.17ZM109.37,214l10.47-52.38a8,8,0,0,0-5-9.06L62,132.71l84.62-90.66L136.16,94.43a8,8,0,0,0,5,9.06l52.8,19.8Z">
+                    </path>
+                  </svg>
+                </div>
+                <div class="text-green-500 text-2xl font-martelsans font-bold break-words">
+                  The future is electric
+                </div>
+              </div>
+              <div class="justify-center items-center flex">
+                <div class="relative">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#22C55E" viewBox="0 0 256 256">
+                    <path
+                      d="M215.79,118.17a8,8,0,0,0-5-5.66L153.18,90.9l14.66-73.33a8,8,0,0,0-13.69-7l-112,120a8,8,0,0,0,3,13l57.63,21.61L88.16,238.43a8,8,0,0,0,13.69,7l112-120A8,8,0,0,0,215.79,118.17ZM109.37,214l10.47-52.38a8,8,0,0,0-5-9.06L62,132.71l84.62-90.66L136.16,94.43a8,8,0,0,0,5,9.06l52.8,19.8Z">
+                    </path>
+                  </svg>
+                </div>
+                <div class="text-green-500 text-2xl font-martelsans font-bold break-words">
+                  The future is electric
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
